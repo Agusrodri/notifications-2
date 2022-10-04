51e3186ca683e08906d5eb1f0c4b9d1c04490150
Agus <agusrodriguez2456@gmail.com>
2022-10-04 00:56:55 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+//import { RestService } from './rest.service';
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  respuesta: any;
+  //usar vapid KEY generada al comienzo
+  readonly VAPID_PUBLIC_KEY = "BIuVxvmtGvdwtH1HEw5VxDN2wmZQk2oZRffwgcdFntG8qgQYoKYDhNmZKHWhNx9tw7lCrYl-wBmT-DjkSq-VEuQ";
+
+  constructor(
+    private swPush: SwPush) {
+
+  }
+
+
+  subscribeToNotifications() {
+    this.swPush.requestSubscription(
+      {
+        serverPublicKey: this.VAPID_PUBLIC_KEY
+      }
+    ).then(respuesta => {
+      this.respuesta = respuesta
+    })
+      .catch(err => {
+        this.respuesta = err
+      })
+
+    // this.swPush.requestSubscription({
+    //   serverPublicKey: this.VAPID_PUBLIC_KEY
+    // }).then(tokens => {
+
+    //   // this.rest.guardarEnviar({
+    //   //   tokens: JSON.parse(JSON.stringify(tokens))
+    //   // }).subscribe(respueta => {
+
+    //   // })
+
+    //   this.respuesta = tokens
+    // })
+    //   .catch(err => {
+    //     this.respuesta = 'ERROR de permiso o navegador no soportado'
+    //   });
+  }
 }
